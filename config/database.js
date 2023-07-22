4323f787aca6dc18e5628845823c84f4add134da
soumyajoshi12 <soumyaj2003@gmail.com>
2023-07-22 11:56:44 -0700

    pass added

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 exports.dataBaseConnection = () => {
   try {
     mongoose.connect(
-      "mongodb+srv://soumyaj2003:<password>@soumya.pibwzbu.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://soumyaj2003:soumyaj2003@soumya.pibwzbu.mongodb.net/ecommerce?retryWrites=true&w=majority"
     );
     console.log("Data Base connected ");
   } catch (error) {
