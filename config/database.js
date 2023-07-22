6962eeca7fcdac1c02eba4ff2bf3c8e2f601cc1f
soumyajoshi12 <soumyaj2003@gmail.com>
2023-07-22 12:00:45 -0700

    data base link changed

--- config/database.js
+++ config/database.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 exports.dataBaseConnection = () => {
   try {
-    mongoose.connect(
-      "mongodb+srv://soumyaj2003:soumyaj2003@soumya.pibwzbu.mongodb.net/ecommerce?retryWrites=true&w=majority"
-    );
+    mongoose.connect("mongodb://localhost:27017/ecommerce");
     console.log("Data Base connected ");
   } catch (error) {
     console.log(error);
